b3e11435918b9c6677dac150ae17c6d6f1f7f63d
guilherme_celente <guilherme.celente@outlook.com>
2025-03-12 16:11:41 -0300

    config(url): configuração de url para acesso a EC2

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://ec2-3-145-166-91.us-east-2.compute.amazonaws.com', // URL do seu backend
+        changeOrigin: true,
+        secure: false, // Defina como false para permitir HTTP no ambiente de desenvolvimento
+        pathRewrite: {
+          '^/api': '/api', // Mantém o caminho '/api' durante a proxyização
+        },
+      },
+    },
+  },
+
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+      // Configuração do Vuetify (se necessário)
+    },
+  },
 })
